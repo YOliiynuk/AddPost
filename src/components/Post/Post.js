dfe7901b6844e08f7d56cf2f2f165b8d1481d13b
YOliiynuk <yurii09oliiynuk@gmail.com>
2023-04-06 16:40:10 +0300

    main

---
+++ src/components/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Post.css';
+
+const Post = () => {
+    return (
+        <div>
+        Post
+        </div>
+    );
+};
+
+export default Post;
+        
---
+++ src/pages/AddPost/AddPost.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AddPost.css';
+
+const AddPost = () => {
+    return (
+        <div>
+        AddPost
+        </div>
+    );
+};
+
+export default AddPost;
+        
